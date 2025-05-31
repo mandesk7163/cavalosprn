3b7d5e3197cb73925f16083efa7e73a129ca3d20
mandesk7163 <mandesk132@gmail.com>
2025-05-31 11:07:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = '.stopware.py';
-    link.download = '.stopware.py';
+    link.href = '.exemplo.py';
+    link.download = '.exemplo.py';
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
 });
